c1966f1cd983a8459513d9f4448074ccc421a22e
Siddharth Mishra <sidmishra@synapseco.com>
2020-02-28 17:39:29 +0530

    resolved conflicts

--- helper/sendEmail.helper.js
+++ helper/sendEmail.helper.js
@@ -1,28 +1,28 @@
 const nodemailer = require("nodemailer");
 
 module.exports = {
-    sendEmail: (userEmail, htmlData) => {
-        return new Promise(resolve => {
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'care@transpero.com',
-                    pass: 'p@ssw0rd1111'
-                }
-            });
+  sendEmail: (userEmail, htmlData) => {
+    return new Promise(resolve => {
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.USER_NAME,
+          pass: process.env.PASSWORD
+        }
+      });
 
-            let mailOptions = {
-                from: 'care@transpero.com', // sender address
-                to: userEmail, // list of receivers
-                subject: `Password update verification`, // Subject line
-                html: htmlData
-            };
+      let mailOptions = {
+        from: process.env.USER_NAME, // sender address
+        to: userEmail, // list of receivers
+        subject: `Password update verification`, // Subject line
+        html: htmlData
+      };
 
-            transporter.sendMail(mailOptions, (error, info) => {
-                console.log(error)
-                if (!info) return resolve('Email Not Sent');
-                return resolve('Email Sent');
-            })
-        })
-    }
+      transporter.sendMail(mailOptions, (error, info) => {
+        console.log(error);
+        if (!info) return resolve("Email Not Sent");
+        return resolve("Email Sent");
+      });
+    });
+  }
 };
